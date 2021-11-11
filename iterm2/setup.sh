bfb0eeb9e284d46aa6bf94a030a38f3b9c28ce6d
Vishnu R Menon <vishthemenon@gmail.com>
2021-11-11 13:24:03 +0000

    Fix iterm config

--- iterm2/setup.sh
+++ iterm2/setup.sh
@@ -3,8 +3,8 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
-. scripts/functions.sh
+. ../scripts/functions.sh
 
 info "Linking iterm2 preferences file to home directory"
 
-ln -s ./com.googlecode.iterm2.plist ~
+symlink ./com.googlecode.iterm2.plist ~/com.googlecode.iterm2.plist
